ca66d804e816c875317de25519c193b4bdfa3a57
musuza.henry <musuzahenry153@gmail.com>
2025-07-27 01:25:09 +0300

    Added relative paths to apis for deployment over proxy for nginx

--- frontend/src/components/Youtubebanner.jsx
+++ frontend/src/components/Youtubebanner.jsx
@@ -28,7 +28,7 @@ function Youtubebanner() {
     const fetchVideos = async()=>{
       try{
         const response = await axios.get('/api/videos/12')//for deployment
-        //const response = await axios.get(`${baseURL}:3000/api/videos/${vids}`); // Assuming you want 10 videos
+        //const response = await axios.get(`${baseURL}:3000/api/videos/${vids}`); //for dev
         setLiveVideos(response.data.liveVideos)
         setNormalVideos(response.data.normalVideos); 
         //alert("hh")
